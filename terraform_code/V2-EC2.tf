1dd08dcafb293f393dda11207c254f11d6802568
joweyel <falsh@protonmail.com>
2024-09-24 22:55:04 +0200

    small change

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -3,31 +3,34 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-  ami           = "ami-0ebfd941bbafe70c6"
-  instance_type = "t2.micro"
-  key_name      = "ddp"
-  security_groups = [ "demo-sg" ]
+    ami = "ami-022e1a32d3f742bd8"
+    instance_type = "t2.micro"
+    key_name = "ddp"
+    security_groups = [ "demo-sg" ]
 }
 
 resource "aws_security_group" "demo-sg" {
-    name = "demo-sg"
-    description = "SSH Access"
+  name        = "demo-sg"
+  description = "SSH Access"
+  
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
 
-    ingress {
-        description = "Ssh access"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = [ "0.0.0.0/0" ]
-        ipv6_cidr_blocks = [ "::/0" ]
-    }
-    tags = {
-        Name = "ssh-prot"
-    }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-prot"
+
+  }
 }
